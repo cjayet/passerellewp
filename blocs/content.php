fafbe4dfe745726d9721464e8d4012c2bebee335
cjayet <c.jayet@optimiz.me>
2016-11-09 17:19:33 +0100

    Update scripts

---
+++ blocs/content.php
@@ -0,0 +1,18 @@
+<h2 id="bloc_content">PUSH content</h2>
+<form action="" method="POST">
+
+    <div class="form-group">
+        <label for="new_title">Nouveau contenu</label>
+        <textarea class="form-control" id="new_content" placeholder="Contenu" value=""></textarea>
+    </div>
+
+    <input type="hidden" name="action" value="set_title" />
+    <button type="button" id="push_content" class="btn btn-default">Envoyer</button>
+</form>
+
+<div class="form-group">
+    <div id="result_update_content" class=""></div>
+</div>
+<br />
+<hr />
+<br />
---
+++ blocs/meta_description.php
@@ -0,0 +1,18 @@
+<h2 id="bloc_meta_description">PUSH meta description</h2>
+<form action="" method="POST">
+
+    <div class="form-group">
+        <label for="new_title">Meta description</label>
+        <textarea class="form-control" id="meta_description" placeholder="Meta description" value=""></textarea>
+    </div>
+
+    <input type="hidden" name="action" value="set_title" />
+    <button type="button" id="push_meta_description" class="btn btn-default">Envoyer</button>
+</form>
+
+<div class="form-group">
+    <div id="result_update_meta_description" class=""></div>
+</div>
+<br />
+<hr />
+<br />
